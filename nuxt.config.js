0ccae27f9ff7fb4620d190ad03339dd5beff69e2
Divine <dpkreativ@gmail.com>
2022-02-15 18:03:39 +0100

    include src to cloudinary product gallery

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://product-gallery.cloudinary.com/all.js',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
