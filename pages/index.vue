715799119ab4bef646fc5281181b0aee79000c6f
Divine <dpkreativ@gmail.com>
2022-02-16 16:22:23 +0100

    update text

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="mx-auto lg:w-96">
+    <div class="my-5">
+      <h2 class="font-bold text-xl">Product Gallery Demo</h2>
+      <p>Click on each image to zoom</p>
+    </div>
     <div id="product-gallery" class="w-full"></div>
-    <h2>Hello Divi</h2>
   </div>
 </template>
 
