40f0ae5c392d6ab5e2a728b7cb52bf365e6a0840
Divine <dpkreativ@gmail.com>
2022-02-17 01:58:10 +0100

    more ui tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mx-auto lg:w-96">
+  <div class="mx-auto w-full max-w-lg p-5">
     <div class="my-5">
       <h2 class="font-bold text-xl">Product Gallery Demo</h2>
-      <p>Click on each image to zoom</p>
+      <p>Click/Tap on each image to zoom</p>
     </div>
     <div id="product-gallery"></div>
   </div>
